f102847579e18d549d8a7137ad4b37f42316ab27
Vasavik1 <97863400+Vasavik1@users.noreply.github.com>
2022-02-06 22:16:33 -0500

    Update Ass4.R

--- Ass4.R
+++ Ass4.R
@@ -19,7 +19,14 @@ boxplot(df$Second ~ df$FinalDecision,
         main='boxplot',
         xlab='Second',
         ylab='FinalDesicion')
-hist(BP,main='histogram of our exp',xlab='BP',ylab='Scale',col='red')
+hist(BP,main='histogram of our exp',xlab='BP',ylab='Scale',col='yellow')
+
+#I have derived a boxplot for the columns Second and Final decision by attaching them to the data frame. Since both the columns has a similar kind of data mostly
+# the obtained box plots are similar.
+
+#I considered Bp column for getting the histogram where the data is distributed accordingly on a scale of 0 to 4.
+ 
+
       
 
 
